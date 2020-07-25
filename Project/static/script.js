bae4a522ab8f763cee5ee9acb6b057ffb2f9a31f
JonECope <info@jonecope.com>
2020-07-25 15:04:07 -0500

    Import Pyrebase added

--- Project/static/script.js
+++ Project/static/script.js
@@ -1,9 +1,9 @@
-var config = {
-    apiKey: "apiKey",
-    authDomain: "projectId.firebaseapp.com",
-    databaseURL: "https://apad-team5.firebaseio.com/",
-    storageBucket: "bucket.appspot.com"
-  };
+var config = config = {
+  apiKey: "AIzaSyBSuBwrJF_Z76sjL0bcUzPXloEOPHFQ5bc",
+  authDomain: "apad-team5.firebaseapp.com",
+  databaseURL: "https://apad-team5.firebaseio.com/",
+  storageBucket: "apad-team5.appspot.com"
+};
   firebase.initializeApp(config);
 
   // Get a reference to the database service
