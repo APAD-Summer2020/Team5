ea796d2897adbeed879333b9ef03df5a45e30364
TheOnlySilence <andyellis888@gmail.com>
2020-07-25 12:18:01 -0500

    main.py starting point1

--- Project/main.py
+++ Project/main.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'admin':
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html',error = "wrong username or password")
+    return  render_template('login.html', error = None)
+    return render_template('login.html')
+
+
+@app.route('/index',methods = ['POST','GET'])
 def index():
-    return render_template('index.html')
+    return render_template('all_themes.html', error = None)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
