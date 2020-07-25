1fe9ca8c4cce1dfaf9978c9de7481c815c3b2f1f
TheOnlySilence <andyellis888@gmail.com>
2020-07-25 14:13:41 -0500

    made the app route work

--- Project/main.py
+++ Project/main.py
@@ -8,24 +8,36 @@ def login():
         username = request.form['username']
         password = request.form['password']
         if username == 'admin' and password == 'admin':
-            return redirect(url_for('all_themes'))
+            return redirect(url_for('manage'))
         else:
             return render_template('login.html',error = "wrong username or password")
     return  render_template('login.html', error = None)
     return render_template('login.html')
 
 
+@app.route('/manage',methods = ['POST','GET'])
+def manage():
+
+    return render_template('manage.html', error = None)
+
+
+@app.route('/create',methods = ['POST','GET'])
+def create():
+
+    return render_template('create.html', error = None)
+
+
 @app.route('/all_themes',methods = ['POST','GET'])
-def all_themes():
-    if request.method == 'POST':
-        if request.form['hometabs'] == "Create Report":
-            return redirect(url_for('create_report'))
+def themes():
 
     return render_template('all_themes.html', error = None)
 
-@app.route('/create_report',methods = ['POST','GET'])
-def index():
-    return render_template('create.html', error = None)
+
+@app.route('/one_theme',methods = ['POST','GET'])
+def search():
+
+    return render_template('one_theme.html', error = None)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
