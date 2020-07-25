269c97f93ccb61ce63552dc07dfd96899d600a3c
TheOnlySilence <andyellis888@gmail.com>
2020-07-25 13:26:14 -0500

    combined create

--- Project/main.py
+++ Project/main.py
@@ -8,17 +8,24 @@ def login():
         username = request.form['username']
         password = request.form['password']
         if username == 'admin' and password == 'admin':
-            return redirect(url_for('index'))
+            return redirect(url_for('all_themes'))
         else:
             return render_template('login.html',error = "wrong username or password")
     return  render_template('login.html', error = None)
     return render_template('login.html')
 
 
-@app.route('/index',methods = ['POST','GET'])
-def index():
+@app.route('/all_themes',methods = ['POST','GET'])
+def all_themes():
+    if request.method == 'POST':
+        if request.form['hometabs'] == "Create Report":
+            return redirect(url_for('create_report'))
+
     return render_template('all_themes.html', error = None)
 
+@app.route('/create_report',methods = ['POST','GET'])
+def index():
+    return render_template('create.html', error = None)
 
 if __name__ == "__main__":
     app.run(debug=True)
