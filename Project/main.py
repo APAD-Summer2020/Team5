182275d27d9426e785a12033ed5c284cf6f87302
Ella Akl <danielle.akl@utexas.edu>
2020-07-26 22:21:28 -0500

    Sign In Works

--- Project/main.py
+++ Project/main.py
@@ -2,60 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, session
 #import pyrebase
 import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
-#from google.cloud import firestore
+
+'''
+Firestore documentation:
+https://firebase.google.com/docs/firestore
+'''
 
 # Use the application default credentials
 cred = credentials.Certificate("gc_privatekey.json")
 firebase_admin.initialize_app(cred)
 db_firestore = firestore.client()
 
-
-'''#DELETE LATER #HARD CODED DATA
-doc_ref = db_firestore.collection(u'users').document(u'admin')
-doc_ref.set({
-    u'username': u'adminaccount',
-    u'email': u'admin@utexas.edu',
-    u'password': 'adminadmin',
-    u'type': 'admin'
-})
-doc_ref = db_firestore.collection(u'users').document(u'regular')
-doc_ref.set({
-    u'username': u'regularaccount',
-    u'email': u'regular@utexas.edu',
-    u'password': 'regreg',
-    u'type': 'regular'
-})'''
-
-
-'''#DELETE #OLD CODE
-config = {
-    "apiKey": "AIzaSyBSuBwrJF_Z76sjL0bcUzPXloEOPHFQ5bc",
-    "authDomain": "apad-team5.firebaseapp.com",
-    "databaseURL": "https://apad-team5.firebaseio.com",
-    "projectId": "apad-team5",
-    "storageBucket": "apad-team5.appspot.com",
-    "messagingSenderId": "311004038430",
-    "appId": "1:311004038430:web:e70bcb7c84b0e96075750f",
-    "measurementId": "G-FSQ5JBDS95"
-}
-
-firebase = pyrebase.initialize_app(config)
-
-db = firebase.database()
-auth = firebase.auth()
-#END OLD CODE'''
-
-
-
-# Creating Keys
-# data = {"AdminAccount": {"username": "admin", "password": "admin"},
-#         "TestUserAccount": {"username": "test", "password": "test"}
-#         }
-#
-# db.child("UserAccounts").set(data)
-
 app = Flask(__name__)
 app.secret_key = "hello"
+
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -80,21 +40,14 @@ def login():
             #If there is, check if the passwords match
             db_pass = doc.get('password')
             if db_pass == password:
-                return render_template('manage.html')
+                #If passwords match, log in.
+                #Send all user information to template.
+                db_username = doc.get('username')
+                db_email = doc.get('email')
+                db_usertype = doc.get('type')
+                return render_template('manage.html', username=db_username, email=db_email, usertype=db_usertype)
         
     return render_template('login.html')
-    
-
-
-""" FOR SEARCHING BY TAG
-From https://dev.to/gogamic/introduction-to-pyrebase-database-2mif
-# reterving data using loops
-all_users = db.child("users").get()
-for user in all_users.each():
-    print(user.key()) # Morty
-    print(user.val()) # {name": "Mortimer 'Morty' Smith"}
-"""
-
 
 
 
@@ -125,30 +78,16 @@ def signup():
     return render_template('signup.html', error=None)
 
 
+
 @app.route('/manage', methods=['POST', 'GET'])
 def manage():
     return render_template('manage.html', error=None)
 
-"""
-To update data for an existing entry use the update() method.
-db.child("users").child("Morty").update({"name": "Mortiest Morty"})
 
-To create your own keys use the set() method. The key in the example below is "Morty".
 
-data = {"name": "Mortimer 'Morty' Smith"}
-db.child("users").child("Morty").set(data)
-
-push
-To save data with a unique, auto-generated, timestamp-based key, use the push() method.
-
-data = {"name": "Mortimer 'Morty' Smith"}
-db.child("users").push(data)
-
-Source:
-https://github.com/thisbejim/Pyrebase
-"""
 @app.route('/create', methods=['POST', 'GET'])
 def create():
+
     user = session["user"]
     if request.method == 'POST':
         themename = request.form['t-name']
@@ -156,26 +95,22 @@ def create():
         data = {"theme-description": themedes}
         db.child("themes").child(themename).set(data)
         return redirect(url_for('create'))
-    #theme = request.form['theme']
-    #reportname = request.form['r-title']
-    #reportdes = request.form['report-description']
-    #reporttag = request.form['r-tag']
-    #data = {"report-description": reportdes, "report-tags":reporttag}
-    #db.child("themes").child(theme).child(reportname).set(data)
 
     return render_template('create.html', username=user)
 
 
+
 @app.route('/all_themes', methods=['POST', 'GET'])
 def themes():
-    all_themes = db.child("themes").get()
-    return render_template('all_themes.html', all_themes=all_themes.val(), error=None)
+    #all_themes = db.child("themes").get()
+    return render_template('all_themes.html', error=None)
 
-# db.child("companies/data").order_by_child("id").equal_to(company_id).limit_to_first(1).get()
-# https://stackoverflow.com/questions/50893423/how-to-get-single-item-in-pyrebase
+
+
 @app.route('/one_theme', methods=['POST', 'GET'])
 def search():
     return render_template('one_theme.html', error=None)
+
 
 
 if __name__ == "__main__":
