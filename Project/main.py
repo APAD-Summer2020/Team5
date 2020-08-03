2332ee2775f4fddb3ec63f0cc45a6e04ae1f9a4c
JonECope <info@jonecope.com>
2020-08-03 16:39:03 -0500

    Map in results page

--- Project/main.py
+++ Project/main.py
@@ -240,8 +240,33 @@ def results():
             #GET DATA STREAM
             posts = db_firestore.collection("posts").where("tags", "array_contains_any", tagsSplit).stream()
 
+            map = Map(
+                identifier="sndmap",
+                lat=37.4419,
+                lng=-122.1419,
+                markers=[
+                  {
 
-            return render_template('results.html', type='tags', posts=posts, usertype=db_usertype)
+                     'lat': 37.4419,
+                     'lng': -122.1419,
+                     'infobox': "<b>Hello World</b>"
+                  },
+                  {
+
+                     'lat': 37.4419,
+                     'lng': -122.1410,
+                     'infobox': "<b>Hello World</b>"
+                  },
+                  {
+
+                     'lat': 37.4300,
+                     'lng': -122.1400,
+                     'infobox': "<b>Hello World from other place</b>"
+                  }
+                ]
+            )
+
+            return render_template('results.html', type='tags', posts=posts, map=map, usertype=db_usertype)
 
         elif 'category' in request.form:
             filterValue = request.form['category']
@@ -255,13 +280,6 @@ def results():
 
 @app.route("/map")
 def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
     sndmap = Map(
         identifier="sndmap",
         lat=37.4419,
@@ -274,6 +292,12 @@ def mapview():
              'infobox': "<b>Hello World</b>"
           },
           {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+             'lat': 37.4419,
+             'lng': -122.1410,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
              'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
              'lat': 37.4300,
              'lng': -122.1400,
@@ -281,7 +305,7 @@ def mapview():
           }
         ]
     )
-    return render_template('map.html', mymap=mymap, sndmap=sndmap)
+    return render_template('map.html', sndmap=sndmap)
 
 if __name__ == "__main__":
     app.run(debug=True)
