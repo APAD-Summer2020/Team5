0bf4fa0fdc0443422f76c11708a3fb23d3ddef5a
Ella Akl <danielle.akl@utexas.edu>
2020-08-13 09:45:25 -0500

    Cleaned up Login and Landing pages

--- CovidCuisineApp/app/src/main/java/com/apadteam5/covidcuisine/Landing.kt
+++ CovidCuisineApp/app/src/main/java/com/apadteam5/covidcuisine/Landing.kt
@@ -1,30 +1,28 @@
 package com.apadteam5.covidcuisine
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_landing.*
+import androidx.appcompat.app.AppCompatActivity
 
 class Landing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
 
-
         val username = intent.getStringExtra("username")
         val createPostButton = findViewById<Button>(R.id.create_post)
+
         createPostButton.setOnClickListener {
             val intent = Intent(this,CreatePost::class.java)
             intent.putExtra("username",username)
             startActivity(intent)
-
         }
+
         val AllCategoriesButton = findViewById<Button>(R.id.all_categories)
         AllCategoriesButton.setOnClickListener {
             val intent = Intent(this,CategoriesMain::class.java)
             startActivity(intent)
-
         }
     }
 }
